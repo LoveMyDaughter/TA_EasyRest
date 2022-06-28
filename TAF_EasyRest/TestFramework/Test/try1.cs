029390dde270a12051c5ceca97edb8ca77b3c587
Tetiana Rezhytska <tania.rezhytska@jmail.com>
2022-06-28 12:19:05 +0300

    add class

---
+++ TAF_EasyRest/TestFramework/Test/try1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFramework.Test
+{
+    internal class try1
+    {
+        //try push
+    }
+}
