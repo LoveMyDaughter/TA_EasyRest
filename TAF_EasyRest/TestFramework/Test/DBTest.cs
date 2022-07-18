18d03decd64036082847cb8586e2db05bec22382
LoveMyDaughter <tsav1986@gmail.com>
2022-07-18 20:25:06 +0300

    Add database connection

---
+++ TAF_EasyRest/TestFramework/Test/DBTest.cs
@@ -0,0 +1,21 @@
+﻿using TestFramework.Tools;
+
+
+namespace TestFramework.Test
+{
+    internal class DBTest
+    {
+        [Test]
+        public void Show_Table_Users()
+        {
+            DBConnectionWrapper.Show_Users();
+        }        
+        
+        [Test]
+        public void Show_Table_User_Roles()
+        {
+            DBConnectionWrapper.Show_User_Roles();
+        }
+
+    }
+}
