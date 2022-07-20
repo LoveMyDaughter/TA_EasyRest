d2091613786bce385bf7a3224f8089ad67fa01f6
sofiaso <sofiaso.sofiasoo@gmail.com>
2022-07-20 23:30:39 +0300

    add ctor & WebElements

---
+++ TAF_EasyRest/TestFramework/Pages/PersonalInfoPage.cs
@@ -0,0 +1,13 @@
+﻿
+namespace TestFramework.Pages
+{
+    public class PersonalInfoPage : BasePage
+    {
+        public PersonalInfoPage(IWebDriver driver) : base(driver) { }
+
+        private IWebElement _personalInfoButton => driver.FindElement(By.XPath("//span[text()='Personal Info']/parent::span/parent::span/parent::a"));
+        private IWebElement _currentOrders => driver.FindElement(By.XPath("//span[text()='Current Orders']/parent::span/parent::span/parent::a"));
+        private IWebElement _orderHistory => driver.FindElement(By.XPath("//span[text()='Order History']/parent::span/parent::span/parent::a"));
+        private IWebElement _myRestaurants => driver.FindElement(By.XPath("//span[text()='My Restaurants']/parent::span/parent::span/parent::a"));
+    }
+}
