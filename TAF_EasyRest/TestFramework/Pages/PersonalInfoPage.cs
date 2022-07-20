c18dc9f2886d1b5bebe8c9809380f1396f4f9b40
sofiaso <sofiaso.sofiasoo@gmail.com>
2022-07-20 23:35:21 +0300

    add click-functions

--- TAF_EasyRest/TestFramework/Pages/PersonalInfoPage.cs
+++ TAF_EasyRest/TestFramework/Pages/PersonalInfoPage.cs
@@ -6,8 +6,32 @@ namespace TestFramework.Pages
         public PersonalInfoPage(IWebDriver driver) : base(driver) { }
 
         private IWebElement _personalInfoButton => driver.FindElement(By.XPath("//span[text()='Personal Info']/parent::span/parent::span/parent::a"));
-        private IWebElement _currentOrders => driver.FindElement(By.XPath("//span[text()='Current Orders']/parent::span/parent::span/parent::a"));
-        private IWebElement _orderHistory => driver.FindElement(By.XPath("//span[text()='Order History']/parent::span/parent::span/parent::a"));
-        private IWebElement _myRestaurants => driver.FindElement(By.XPath("//span[text()='My Restaurants']/parent::span/parent::span/parent::a"));
+        private IWebElement _currentOrdersButton => driver.FindElement(By.XPath("//span[text()='Current Orders']/parent::span/parent::span/parent::a"));
+        private IWebElement _orderHistoryButton => driver.FindElement(By.XPath("//span[text()='Order History']/parent::span/parent::span/parent::a"));
+        private IWebElement _myRestaurantsButton => driver.FindElement(By.XPath("//span[text()='My Restaurants']/parent::span/parent::span/parent::a"));
+
+        public PersonalInfoPage PersonalInfoButtonClick()
+        {
+            _personalInfoButton.Click();
+            return this;
+        }
+
+        public PersonalInfoPage СurrentOrdersButtonClick()
+        {
+            _currentOrdersButton.Click();
+            return this;
+        }
+
+        public PersonalInfoPage OrderHistoryButtonClick()
+        {
+            _orderHistoryButton.Click();
+            return this;
+        }
+
+        public PersonalInfoPage MyRestaurantsButtonClick()
+        {
+            _myRestaurantsButton.Click();
+            return this;
+        }
     }
 }
