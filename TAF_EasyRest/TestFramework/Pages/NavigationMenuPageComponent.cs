382da28b08111239f0b84cbc365e6096fe49805c
andriifromlviv <a.shalavylo@gmail.com>
2022-07-21 18:21:37 +0300

    AddInheritance

--- TAF_EasyRest/TestFramework/Pages/NavigationMenuPageComponent.cs
+++ TAF_EasyRest/TestFramework/Pages/NavigationMenuPageComponent.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace TestFramework.Pages
 {
-    internal class NavigationMenuPageComponent
+    internal class NavigationMenuPageComponent : BasePage
     {
+        private IWebElement _EasyRestButton => driver.FindElement(By.XPath("//a[text()='Easy-rest']"));
+        private IWebElement _HomeButton => driver.FindElement(By.XPath("//span[text()='Home']"));
+        private IWebElement _RestaurantsList => driver.FindElement(By.XPath("//span[text()='Restaurants List']"));
+        private IWebElement _SignIn => driver.FindElement(By.XPath("//span[text()='Sign In']"));
+        private IWebElement _SignUp => driver.FindElement(By.XPath("//span[text()='Sign Up']"));
+
+        public NavigationMenuPageComponent(IWebDriver driver) : base(driver) { }
+
+        public NavigationMenuPageComponent ClickEasyRestButton()
+        {
+            _EasyRestButton.Click();
+            return this;
+        }
+
+        public NavigationMenuPageComponent ClickHomeButton()
+        {
+            _HomeButton.Click();
+            return this;
+        }
+
+        public NavigationMenuPageComponent ClickRestaurantsListButton()
+        {
+            _RestaurantsList.Click();
+            return this;
+        }
+
+        public NavigationMenuPageComponent ClickSignInListButton()
+        {
+            _SignIn.Click();
+            return this;
+        }
+
+        public NavigationMenuPageComponent ClickSignUpButton()
+        {
+            _SignUp.Click();
+            return this;
+        }
+
+
     }
 }
