bc44a69aab1c442a204a1e7fa2b28e59b9f65890
LoveMyDaughter <tsav1986@gmail.com>
2022-07-22 17:22:42 +0300

    Add page elements

--- TAF_EasyRest/TestFramework/Pages/AdminPanelCreateModeratorPage.cs
+++ TAF_EasyRest/TestFramework/Pages/AdminPanelCreateModeratorPage.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestFramework.Pages
+﻿namespace TestFramework.Pages
 {
-    internal class AdminPanelCreateModeratorPage
+    public class AdminPanelCreateModeratorPage : BasePage
     {
+        public AdminPanelPageComponent adminPanelPageComponent { get; }
+        public AdminPanelCreateModeratorPage (IWebDriver driver) : base(driver)
+        {
+            adminPanelPageComponent = new AdminPanelPageComponent(driver);
+        }
+
+        #region Elements
+        private IWebElement _nameField => driver.FindElement(By.XPath("//input[@name='name']"));
+        private IWebElement _emailField => driver.FindElement(By.XPath("//input[@name='email']"));
+        private IWebElement _phoneNumberField => driver.FindElement(By.XPath("//input[@name='phoneNumber']"));
+        private IWebElement _birthDateField => driver.FindElement(By.XPath("//input[@name='birthDate']"));
+        private IWebElement _passwordField => driver.FindElement(By.XPath("//input[@name='password']"));
+        private IWebElement _repeatedPasswordField => driver.FindElement(By.XPath("//input[@name='repeated_password']"));
+        private IWebElement _cancelButton => driver.FindElement(By.XPath("//span[text() = 'Cancel']/parent::a"));
+        private IWebElement _createAccountButton => driver.FindElement(By.XPath("//span[text() = 'Create account']/parent::button"));
+
+
+        #endregion
+
+
+
+        #region Methods
+
+
+        #endregion
+
     }
 }
