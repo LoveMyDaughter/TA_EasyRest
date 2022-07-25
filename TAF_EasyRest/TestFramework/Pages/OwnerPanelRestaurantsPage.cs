e015e9e95f4ad375a1e2032a78a42ab683c07b73
LoveMyDaughter <tsav1986@gmail.com>
2022-07-26 01:11:40 +0300

    Add OwnerPanelRestaurantsPage

---
+++ TAF_EasyRest/TestFramework/Pages/OwnerPanelRestaurantsPage.cs
@@ -0,0 +1,52 @@
+﻿using TestFramework.PagesComponents;
+
+namespace TestFramework.Pages
+{
+    public class OwnerPanelRestaurantsPage : BasePage
+    {
+        public NavigationMenuPageComponent NavigationMenuPageComponent { get; }
+        public PersonalInfoPageComponent PersonalInfoPageComponent { get; }
+
+        public OwnerPanelRestaurantsPage(IWebDriver driver) : base(driver)
+        {
+            PersonalInfoPageComponent = new PersonalInfoPageComponent(driver);
+            NavigationMenuPageComponent = new NavigationMenuPageComponent(driver);
+        }
+
+
+        #region Elements
+
+        // This button is bind to not archived restaurants. Otherwise we can't archive the restaurant
+        private IWebElement _threeDotButton => driver.FindElement(By.XPath("//span[text() != 'ARCHIVED']/ancestor::div/button"));
+
+        // For this buttons: use wait until threeDotButton expands
+        private IWebElement _manageButton => driver.FindElement(By.XPath("//span[text() = 'Manage']/ancestor::a"));
+        private IWebElement _archiveButton => driver.FindElement(By.XPath("//span[text() = 'Archive']/ancestor::li"));
+
+        #endregion
+
+
+        #region Methods
+
+        public OwnerPanelRestaurantsPage ClickThreeDotButton()
+        {
+            _threeDotButton.Click();
+            return this;
+        }
+
+        public OwnerPanelRestaurantsPage ClickArchiveButton()
+        {
+            _archiveButton.Click();
+            return this;
+        }
+
+        public OwnerPanelRestaurantsPage ClickManageButton()
+        {
+            _manageButton.Click();
+            return this;
+        }
+
+        #endregion
+
+    }
+}
