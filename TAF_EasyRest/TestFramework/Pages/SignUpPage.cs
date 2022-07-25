c2a6d873158e736417ff1df45692b8f13269ada0
andriifromlviv <a.shalavylo@gmail.com>
2022-07-25 16:06:48 +0300

    create class, add web elements

---
+++ TAF_EasyRest/TestFramework/Pages/SignUpPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFramework.Pages
+{
+    internal class SignUpPage : BasePage
+    {
+        public NavigationMenuPageComponent NavigationMenu { get; set; }
+
+        public SignUpPage(IWebDriver driver) : base(driver)
+        {
+            NavigationMenu = new NavigationMenuPageComponent(driver);
+        }
+
+        private IWebElement _nameField => driver.FindElement(By.XPath("//input [@name='name']"));
+        private IWebElement _emailField => driver.FindElement(By.XPath("//input [@name='email']"));
+        private IWebElement _phoneNumberField => driver.FindElement(By.XPath("//input [@name='phoneNumber']"));
+        private IWebElement _birthDateField => driver.FindElement(By.XPath("//input [@name='birthDate']"));
+        private IWebElement _passwordField => driver.FindElement(By.XPath("//input [@name='password']"));
+        private IWebElement _confirmPasswordField => driver.FindElement(By.XPath("//input [@name='repeated_password']"));
+        private IWebElement _createAccountButton => driver.FindElement(By.XPath("//button[@type='submit']"));
+    }
+}
