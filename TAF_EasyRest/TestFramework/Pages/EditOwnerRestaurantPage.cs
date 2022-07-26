91027cec3d3591dc655c23e2fd0d0eda2f73b0b9
Tetiana Rezhytska <tania.rezhytska@jmail.com>
2022-07-26 15:38:53 +0300

    Start class

---
+++ TAF_EasyRest/TestFramework/Pages/EditOwnerRestaurantPage.cs
@@ -0,0 +1,31 @@
+﻿using TestFramework.PageComponents;
+namespace TestFramework.Pages;
+
+public class OwnerEditRestaurantPage : BasePage
+{
+    public OwnerEditRestaurantPage(IWebDriver driver) : base(driver)
+    {
+        ManageRestaurantPageComponent = new ManageRestaurantPageComponent(driver);
+    }
+    public ManageRestaurantPageComponent ManageRestaurantPageComponent { get; }
+
+    private IWebElement _editRestaurantButton => driver.FindElement(By.XPath("//*[@id='root']/div/main/div[2]/button"));
+    private IWebElement _updateRestaurantButton => driver.FindElement(By.XPath("//span[contains(text(),'Update')]/parent::button"));
+    private IWebElement _cancelEditRestaurantButton => driver.FindElement(By.XPath("//span[contains(text(),'Cancel')]/parent::button"));
+
+    public ManageRestaurantPage ClickEditRestaurantButton()
+    {
+        _editRestaurantButton.Click();
+        return this;
+    }
+    public ManageRestaurantPage UpdateEditRestaurantButton()
+    {
+        _updateRestaurantButton.Click();
+        return this;
+    }
+    public ManageRestaurantPage CancelRestaurantButton()
+    {
+        _cancelEditRestaurantButton.Click();
+        return this;
+    }
+}
