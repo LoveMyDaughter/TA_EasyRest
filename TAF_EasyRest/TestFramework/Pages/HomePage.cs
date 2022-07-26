bfb256bda256c5f4535d9a750a00010618a99f0d
andriifromlviv <a.shalavylo@gmail.com>
2022-07-26 16:12:29 +0300

    add changes

--- TAF_EasyRest/TestFramework/Pages/HomePage.cs
+++ TAF_EasyRest/TestFramework/Pages/HomePage.cs
@@ -1,10 +1,10 @@
-﻿using TestFramework.PagesComponents;
+﻿using TestFramework.PageComponents;
 
 namespace TestFramework.Pages
 {
     internal class HomePage : BasePage
     {
-        public NavigationMenuPageComponent NavigationMenu { get; set; }
+        public NavigationMenuPageComponent NavigationMenu { get; }
 
         public HomePage(IWebDriver driver) : base(driver)
         {
