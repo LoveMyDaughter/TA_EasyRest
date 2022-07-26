bb4f043e24a173dd185a8a9a54b767e808117f40
Tetiana Rezhytska <tania.rezhytska@jmail.com>
2022-07-26 15:56:44 +0300

    Add class OwnerEditRestaurant

--- TAF_EasyRest/TestFramework/Pages/OwnerEditRestaurantPage.cs
+++ TAF_EasyRest/TestFramework/Pages/OwnerEditRestaurantPage.cs
@@ -6,24 +6,26 @@ public class OwnerEditRestaurantPage : BasePage
     public OwnerEditRestaurantPage(IWebDriver driver) : base(driver)
     {
         ManageRestaurantPageComponent = new ManageRestaurantPageComponent(driver);
+        NavigationMenuPageComponent = new NavigationMenuPageComponent(driver);
     }
     public ManageRestaurantPageComponent ManageRestaurantPageComponent { get; }
+    public NavigationMenuPageComponent NavigationMenuPageComponent { get; }
 
-    private IWebElement _editRestaurantButton => driver.FindElement(By.XPath("//*[@id='root']/div/main/div[2]/button"));
+    private IWebElement _editRestaurantButton => driver.FindElement(By.XPath("//button[@title = 'Edit Information']"));
     private IWebElement _updateRestaurantButton => driver.FindElement(By.XPath("//span[contains(text(),'Update')]/parent::button"));
     private IWebElement _cancelEditRestaurantButton => driver.FindElement(By.XPath("//span[contains(text(),'Cancel')]/parent::button"));
 
-    public ManageRestaurantPage ClickEditRestaurantButton()
+    public OwnerEditRestaurantPage ClickEditRestaurantButton()
     {
         _editRestaurantButton.Click();
         return this;
     }
-    public ManageRestaurantPage UpdateEditRestaurantButton()
+    public OwnerEditRestaurantPage UpdateEditRestaurantButton()
     {
         _updateRestaurantButton.Click();
         return this;
     }
-    public ManageRestaurantPage CancelRestaurantButton()
+    public OwnerEditRestaurantPage CancelRestaurantButton()
     {
         _cancelEditRestaurantButton.Click();
         return this;
