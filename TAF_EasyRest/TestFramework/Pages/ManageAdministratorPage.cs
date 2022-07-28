42e36d51ededfe4f02083478679c7a3ce566b53e
Tetiana Rezhytska <tania.rezhytska@jmail.com>
2022-07-28 07:16:03 +0300

    Add CreateNewAdministrator

---
+++ TAF_EasyRest/TestFramework/Pages/ManageAdministratorPage.cs
@@ -0,0 +1,22 @@
+﻿using TestFramework.PageComponents;
+namespace TestFramework.Pages
+{
+    public class ManageAdministratorPage : BasePage
+    {
+        public ManageAdministratorPage(IWebDriver driver) : base(driver)
+        {
+            ManageRestaurantPageComponent = new ManageRestaurantPageComponent(driver);
+        }
+        public ManageRestaurantPageComponent ManageRestaurantPageComponent { get; }
+
+        private IWebElement _addAdministratorButton => driver.FindElement(By.XPath("//button[@title = 'Add Administrator"));
+
+
+        public CreateNewAdministratorPageComponent ClickAddAdministratorButton()
+        {
+            _addAdministratorButton.Click();
+            return new CreateNewAdministratorPageComponent(driver);
+        }
+
+    }
+}
