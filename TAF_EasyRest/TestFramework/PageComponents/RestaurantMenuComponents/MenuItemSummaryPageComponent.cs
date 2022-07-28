f059bb9c12cf5b982386fac87b7415a6923d2dc7
andriifromlviv <a.shalavylo@gmail.com>
2022-07-28 13:31:21 +0300

    add possibility to navigate to different component items

--- TAF_EasyRest/TestFramework/PageComponents/RestaurantMenuComponents/MenuItemSummaryPageComponent.cs
+++ TAF_EasyRest/TestFramework/PageComponents/RestaurantMenuComponents/MenuItemSummaryPageComponent.cs
@@ -3,14 +3,16 @@
     public class MenuItemSummaryPageComponent
     {
         IWebDriver driver { get; }
+        private int index;
 
-        public MenuItemSummaryPageComponent(IWebDriver driver)
+        public MenuItemSummaryPageComponent(IWebDriver driver, int index)
         {
             this.driver = driver;
+            this.index = index;
         }
 
-        private IWebElement _expandButton => driver.FindElement(By.XPath("//button[@class='MuiButtonBase - root - 106 MuiIconButton - root - 1576 MenuItem - expandButton - 1988']"));
-        private IWebElement _addToCartButton => driver.FindElement(By.XPath("//button[@class='MuiButtonBase-root-106 MuiIconButton-root-1576 MenuItem-addButton-1991']"));
+        private IWebElement _expandButton => driver.FindElement(By.XPath($"(//button[@class='MuiButtonBase-root-106 MuiIconButton-root-1576 MenuItem-expandButton-2489'])[{index}]"));
+        private IWebElement _addToCartButton => driver.FindElement(By.XPath($"(//button[@class='MuiButtonBase-root-106 MuiIconButton-root-1576 MenuItem-addButton-2492'])[{index}]"));
 
         public MenuItemDetailsPageComponent ClickExpandButton()
         {
