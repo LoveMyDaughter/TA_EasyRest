1c073eb4302606e38f52f95bdc0da059b8704d33
sofiaso <sofiaso.sofiasoo@gmail.com>
2022-07-28 15:20:45 +0300

    change button xpath

--- TAF_EasyRest/TestFramework/PageComponents/WaitingForConfirmOrderDetailsPageComponent.cs
+++ TAF_EasyRest/TestFramework/PageComponents/WaitingForConfirmOrderDetailsPageComponent.cs
@@ -3,13 +3,14 @@
     public class WaitingForConfirmOrderDetailsPageComponent
     {
         private IWebDriver driver;
-
-        public WaitingForConfirmOrderDetailsPageComponent(IWebDriver driver)
+        private int index;
+        public WaitingForConfirmOrderDetailsPageComponent(IWebDriver driver, int index)
         {
             this.driver = driver;
+            this.index = index;
         }
 
-        private IWebElement _declineButton => driver.FindElement(By.XPath("(//div[@class='UserOrders-root-2528']/child::div)[1]//button"));
+        private IWebElement _declineButton => driver.FindElement(By.XPath($"(//button[@class='MuiButtonBase-root-106 MuiButton-root-80 MuiButton-contained-91 MuiButton-containedPrimary-92 MuiButton-raised-94 MuiButton-raisedPrimary-95'])[{index}]"));
 
         public WaitingForConfirmOrderDetailsPageComponent ClickDeclineButton()
         {
