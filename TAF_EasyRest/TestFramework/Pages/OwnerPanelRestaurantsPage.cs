a899f18bca0c135d6e735bd7c598fe4a111979d0
Tetiana Rezhytska <tania.rezhytska@jmail.com>
2022-07-28 12:04:07 +0300

    Changes

--- TAF_EasyRest/TestFramework/Pages/OwnerPanelRestaurantsPage.cs
+++ TAF_EasyRest/TestFramework/Pages/OwnerPanelRestaurantsPage.cs
@@ -1,13 +1,16 @@
 ﻿using TestFramework.PageComponents;
+using TestFramework.PageComponents.NavigationMenuComponents;
+
 namespace TestFramework.Pages;
 public class OwnerPanelRestaurantsPage : BasePage
 {
 
     public PersonalInfoPageComponent PersonalInfoPageComponent { get; }
-
+    public NavigationMenuPageComponent NavigationMenuPageComponent { get; }
     public OwnerPanelRestaurantsPage(IWebDriver driver) : base(driver)
     {
         PersonalInfoPageComponent = new PersonalInfoPageComponent(driver);
+        NavigationMenuPageComponent = new NavigationMenuPageComponent(driver);
     }
 
     private IWebElement _threeDotButton => driver.FindElement(By.XPath("//span[text() != 'ARCHIVED']/ancestor::div/button"));
