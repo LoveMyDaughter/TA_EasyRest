74ff7cda153312deae29ba6ef7f85ad356ebf677
andriifromlviv <a.shalavylo@gmail.com>
2022-08-02 16:32:34 +0300

    add page object class and page components

---
+++ TAF_EasyRest/TestFramework/PageComponents/WaitingForConfirmTabPageComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFramework.PageComponents
+{
+    public class WaitingForConfirmTabPageComponent
+    {
+        IWebDriver driver;
+        private IWebElement _ordersContainer => driver.FindElement(By.XPath("//body/div/main/div/div/div/div"));
+        private IReadOnlyCollection<IWebElement> _orders => _ordersContainer.FindElements(By.XPath("/div"));
+
+        public WaitingForConfirmTabPageComponent(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public WaitingForConfirmOrderCardPageComponent ExpandAnyOrder()
+        {
+            var card = _orders.ElementAt(0);
+
+            var expandButton = card.FindElement(By.XPath("//button"));
+            expandButton.Click();
+
+            return new WaitingForConfirmOrderCardPageComponent(driver);
+        }
+    }
+}
---
+++ TAF_EasyRest/TestFramework/Pages/AdministratorPanelPage.cs
@@ -0,0 +1,34 @@
+﻿using TestFramework.PageComponents;
+using TestFramework.PageComponents.NavigationMenuComponents;
+
+namespace TestFramework.Pages
+{
+    public class AdministratorPanelPage : BasePage
+    {
+        NavigationMenuPageComponent NavigationMenuPageComponent { get; }
+        UserMenuHeaderButtonPageComponent UserMenuHeaderButton { get; }
+        
+
+        public AdministratorPanelPage(IWebDriver driver) : base(driver)
+        {
+            NavigationMenuPageComponent NavigationMenuPageComponent = new NavigationMenuPageComponent(driver);
+            UserMenuHeaderButtonPageComponent UserMenuHeaderButton = new UserMenuHeaderButtonPageComponent(driver);
+        }
+
+        private IWebElement _waitingForConfirmButton => driver.FindElement(By.XPath("//header//span[text()='Waiting for confirm']"));
+        private IWebElement _acceptedButton => driver.FindElement(By.XPath("//header//span[text()='Accepted']"));
+
+        public WaitingForConfirmTabPageComponent ClickWaitingForConfirmButton()
+        {
+            _waitingForConfirmButton.Click();
+            return new WaitingForConfirmTabPageComponent(driver);
+        }
+
+        public AcceptedTabPageComponent ClickAcceptedButton()
+        {
+            _acceptedButton.Click();
+            return new AcceptedTabPageComponent(OrdersContainer);
+        }
+
+    }
+}
