aca5aa85965e788f6f2a7f47e0ab84c965f917d5
LoveMyDaughter <tsav1986@gmail.com>
2022-08-02 10:38:07 +0300

    Correct AdminPanel_ModeratorsPage

--- TAF_EasyRest/TestFramework/Pages/AdminPanelModeratorsPage.cs
+++ TAF_EasyRest/TestFramework/Pages/AdminPanelModeratorsPage.cs
@@ -6,41 +6,57 @@ namespace TestFramework.Pages
     public class AdminPanelModeratorsPage : BasePage
     {
 
-        public NavigationMenuPageComponent NavigationMenuPageComponent { get; }
-        public AdminPanelPageComponent AdminPanelPageComponent { get; }
-
-
-        private IWebElement _addModeratorButton => driver.FindElement(By.XPath("//*[@id = 'root']/div/main/a"));
-
-        // Corresponding entry button. It's better to find 1-st row instead of certain user name. Like //tbody/tr[1]
-        private IWebElement _padlockButton => driver.FindElement(By.XPath("//th[text()='Peter Moderator']/following-sibling::td/button"));
-        private IWebElement _statusRecord => driver.FindElement(By.XPath("//th[text()='Peter Moderator']/following-sibling::td/p"));
-        private IWebElement _padlockButtonForCertainUser(string username) => driver.FindElement(By.XPath($"//th[text()='{username}']/following-sibling::td/button"));
-
+        public AdminPanelPageComponent AdminLeftsideMenu { get; }
+        public NavigationMenuPageComponent NavigationMenu { get; }
+        public UserMenuHeaderButtonPageComponent UserButton { get; }
 
 
         public AdminPanelModeratorsPage(IWebDriver driver) : base(driver)
         {
-            AdminPanelPageComponent = new AdminPanelPageComponent(driver);
-            NavigationMenuPageComponent = new NavigationMenuPageComponent(driver);
+            AdminLeftsideMenu = new AdminPanelPageComponent(driver);
+            NavigationMenu = new NavigationMenuPageComponent(driver);
+            UserButton = new UserMenuHeaderButtonPageComponent(driver);
         }
 
-        public AdminPanelModeratorsPage ClickPadlockButton()
+
+        #region Elements
+
+        private IWebElement _addModeratorButton => driver.FindElement(By.XPath("//*[@id = 'root']/div/main/a"));
+
+        // Table body with the list of moderators. This is a parent for any moderator line
+        private IWebElement _moderatorsList => driver.FindElement(By.XPath("//tbody"));
+
+
+        // These elements are only available inside the _moderatorsList component
+        private IWebElement _statusRecord(string username) => driver.FindElement(By.XPath($"//th[text()='{username}']/following-sibling::td/p"));
+        private IWebElement _padlockButtonForUser(string username) => driver.FindElement(By.XPath($"//th[text()='{username}']/following-sibling::td/button"));
+
+        #endregion
+
+
+        #region Methods
+
+        public AdminPanelModeratorsPage ClickPadlockButton(string username)
         {
-            _padlockButton.Click();
+            _padlockButtonForUser(username).Click();
             return this;
+            // Add exception: User not found
         }
 
-        public AdminPanelModeratorsPage ClickAddModeratorButton()
+        // "Active" or "Banned" status for required user - to check if user's activity changes
+        public string StatusRecordGetText(string username)
+        {
+            return _statusRecord(username).Text;
+            // Add exception: User not found
+        }
+
+        public AdminPanelCreateModeratorPage ClickAddModeratorButton()
         {
             _addModeratorButton.Click();
-            return this;
+            return new AdminPanelCreateModeratorPage(driver);
         }
 
-        public string StatusRecordGetText()
-        {
-            return _statusRecord.Text;
-        }
+        #endregion
 
 
     }
