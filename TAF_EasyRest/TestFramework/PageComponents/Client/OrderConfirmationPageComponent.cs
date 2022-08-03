df3534f11110a411b3988bd861e419f4e73f6285
sofiaso <sofiaso.sofiasoo@gmail.com>
2022-08-03 17:57:35 +0300

    add ConfirmatioPage

---
+++ TAF_EasyRest/TestFramework/PageComponents/Client/OrderConfirmationPageComponent.cs
@@ -0,0 +1,46 @@
+﻿using TestFramework.PageComponents;
+namespace TestFramework.Pages
+{
+    public class OrderConfirmationPageComponent : BasePage
+    {
+        public OrderConfirmationPageComponent(IWebDriver driver) : base(driver)
+        {
+            FillOdersList();
+        }
+
+        #region Elements
+        public List<OrderSummaryDetailsPageComponent> _summaryDetails { get; private set; }
+        private IWebElement _cancelButton => driver.FindElement(By.XPath("//span[text()='Cancel']//parent::button"));
+        private IWebElement _submitButton => driver.FindElement(By.XPath("//span[text()='Submit']//parent::button"));
+        #endregion
+
+        #region Methods
+        private int СountOrders()
+        {
+            IReadOnlyCollection<IWebElement> _summaryDetails = driver.FindElements(By.XPath("//td[@class='MuiTableCell-root-3018 MuiTableCell-body-3020 MuiTableCell-paddingCheckbox-3024']//parent::tr"));
+            return _summaryDetails.Count();
+        }
+
+        private void FillOdersList()
+        {
+            _summaryDetails = new List<OrderSummaryDetailsPageComponent>(СountOrders());
+            for (int i = 0; i < _summaryDetails.Count; i++)
+            {
+                _summaryDetails.Add(new OrderSummaryDetailsPageComponent(driver, (i + 1)));
+            }
+        }
+
+        public OrderConfirmationPageComponent ClickCancelButton()
+        {
+            _cancelButton.Click();
+            return new OrderConfirmationPageComponent(driver);
+        }
+
+        public OrderConfirmationPageComponent ClickSubmitButton()
+        {
+            _submitButton.Click();
+            return new OrderConfirmationPageComponent(driver);
+        }
+        #endregion
+    }
+}
---
+++ TAF_EasyRest/TestFramework/PageComponents/Client/OrderSummaryDetailsPageComponent.cs
@@ -0,0 +1,35 @@
+﻿namespace TestFramework.PageComponents
+{
+    public class OrderSummaryDetailsPageComponent
+    {
+        private IWebDriver driver;
+        private int index;
+
+        public OrderSummaryDetailsPageComponent(IWebDriver driver, int index)
+        {
+            this.driver = driver;
+            this.index = index;
+        }
+
+        #region Elements
+        private IWebElement _currentElement => driver.FindElement(By.XPath($"(//td[@class='MuiTableCell-root-3326 MuiTableCell-body-3328 MuiTableCell-paddingCheckbox-3332']//parent::tr)[{index}]"));
+        private IWebElement _removeButton => _currentElement.FindElement(By.XPath(".//button[@aria-label='Remove item']"));
+        private IWebElement _quantityField => _currentElement.FindElement(By.XPath(""));
+        #endregion
+
+        #region Methods
+        public OrderConfirmationPageComponent ClickRemoveButton()
+        {
+            _removeButton.Click();
+            return new OrderConfirmationPageComponent(driver);
+        }
+
+        public OrderConfirmationPageComponent SendKeysRemoveButton(string keys)
+        {
+            _quantityField.Click();
+            _quantityField.SendKeys(keys);
+            return new OrderConfirmationPageComponent(driver);
+        }
+        #endregion
+    }
+}
