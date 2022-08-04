ffa8be021e6bbbd9f3d309d2cc3a8b8bac7b0cc7
Tetiana Rezhytska <tania.rezhytska@jmail.com>
2022-08-04 12:24:59 +0300

    Add CreateMenuPage and component CreateListMenu

---
+++ TAF_EasyRest/TestFramework/PageComponents/Owner/CreateListMenuComponent.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace TestFramework.PageComponents.Owner
+{
+    public class CreateListMenuComponent
+    {
+        IWebDriver driver { get; }
+
+        public CreateListMenuComponent(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+    }
+
+}
---
+++ TAF_EasyRest/TestFramework/Pages/Owner/CreateMenuPage.cs
@@ -0,0 +1,43 @@
+﻿using TestFramework.PageComponents;
+using TestFramework.PageComponents.NavigationMenuComponents;
+
+namespace TestFramework.Pages.Owner
+{
+    internal class CreateMenuPage : BasePage
+    {
+        public NavigationMenuPageComponent NavigationMenuPageComponent { get; }
+        public ManageRestaurantPageComponent ManageRestaurantPageComponent { get; }
+
+        public CreateMenuPage(IWebDriver driver) : base(driver)
+        {
+            NavigationMenuPageComponent = new NavigationMenuPageComponent(driver);
+            ManageRestaurantPageComponent = new ManageRestaurantPageComponent(driver);
+        }
+        private IWebElement _menuNameField => driver.FindElement(By.Name("Menu Name"));
+        private IWebElement _listMenuButton => driver.FindElement(By.XPath("//span[text() = 'List menu']"));
+        private IWebElement _imageMenuButton => driver.FindElement(By.XPath("//span[text() = 'List menu']"));
+        private IWebElement _nextButton => driver.FindElement(By.XPath(" //span[text() = 'Next']"));
+
+        public CreateMenuPage ClickMenuField()
+        {
+            _menuNameField.Click();
+            return this;
+        }
+        public CreateMenuPage ClickListMenuButton()
+        {
+            _listMenuButton.Click();
+            return this;
+        }
+        public CreateMenuPage ClickImageMenuButton()
+        {
+            _imageMenuButton.Click();
+            return this;
+        }
+        public CreateMenuPage ClickNextButton()
+        {
+            _nextButton.Click();
+            return this;
+        }
+    }
+}
+
