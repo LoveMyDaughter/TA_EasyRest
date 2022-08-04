c57635d8fcd92b7ba1628d7ad338de11e6c44d0c
LoveMyDaughter <tsav1986@gmail.com>
2022-08-04 17:32:32 +0300

    Create components for Archived and Unapproved tabs

---
+++ TAF_EasyRest/TestFramework/PageComponents/Moderator/UnapprovedRestaurantsPageComponent .cs
@@ -0,0 +1,37 @@
+﻿namespace TestFramework.PageComponents.Moderator
+{
+    public class UnapprovedRestaurantsPageComponent
+    {
+        private IWebDriver driver;
+
+        public UnapprovedRestaurantsPageComponent(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        private IReadOnlyCollection<IWebElement> _restaurantsGrid => driver.FindElements(By.XPath("//div[contains(@class, 'Grid-item')]"));
+
+
+
+        private IWebElement _approveButton => driver.FindElement(By.XPath("//span[text() = 'Approve']/parent::button"));
+
+        public UnapprovedRestaurantsPageComponent ClickApproveButton()
+        {
+            var firstRestaurantFromGrid = _restaurantsGrid.ElementAt(0);
+            var approveButton = firstRestaurantFromGrid.FindElement(By.XPath(".//span[text() = 'Approve']/parent::button"));
+
+            approveButton.Click();
+
+            return this;
+        }
+
+
+
+
+        public ArchivedRestaurantsPageComponent FindAndClickRestoreButton()
+        {
+
+        }
+
+    }
+}
