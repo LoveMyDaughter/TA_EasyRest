03283979b85efe4ad58334e55f0ef7fc2dc8480e
Tetiana Rezhytska <tania.rezhytska@jmail.com>
2022-08-05 00:45:30 +0300

    Add second component for CreateMenuPage

---
+++ TAF_EasyRest/TestFramework/PageComponents/Owner/CreateMenuPageComponents/CreateListMenuComponent.cs
@@ -0,0 +1,37 @@
+﻿using TestFramework.Pages.Owner;
+
+namespace TestFramework.PageComponents.Owner.CreateMenuPageComponents
+{
+    public class CreateListMenuComponent 
+    {
+        IWebDriver driver { get; }
+
+        public CreateListMenuComponent(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+  
+        private IWebElement _threeDotsButton => driver.FindElement(By.XPath(".//button[@aria-label='Action']"));
+        private IWebElement _nextButton => driver.FindElement(By.XPath(".//button[@aria-label='Action']/parent::div/parent::div/parent::div/parent::div/following-sibling::div[2]"));
+        private IWebElement _backButton => driver.FindElement(By.XPath(".//button[@aria-label='Action']/parent::div/parent::div/parent::div/parent::div/following-sibling::div[1]"));
+
+        public CreateListMenuComponent ClickThreeDotsButton()
+        {
+            _threeDotsButton.Click();
+            return this;
+        }
+        public CreateListMenuComponent ClickNextButton()
+        {
+            _nextButton.Click();
+            return this;
+        }
+
+        public CreateMenuPage ClickBackButton(
+            )
+        {
+            _backButton.Click();
+            return new CreateMenuPage(driver);
+        }
+    }
+
+}
--- TAF_EasyRest/TestFramework/Pages/Owner/CreateMenuPage.cs
+++ TAF_EasyRest/TestFramework/Pages/Owner/CreateMenuPage.cs
@@ -1,9 +1,10 @@
 ﻿using TestFramework.PageComponents;
 using TestFramework.PageComponents.NavigationMenuComponents;
+using TestFramework.PageComponents.Owner.CreateMenuPageComponents;
 
 namespace TestFramework.Pages.Owner
 {
-    internal class CreateMenuPage : BasePage
+    public class CreateMenuPage : BasePage
     {
         public NavigationMenuPageComponent NavigationMenuPageComponent { get; }
         public ManageRestaurantPageComponent ManageRestaurantPageComponent { get; }
@@ -15,12 +16,17 @@ namespace TestFramework.Pages.Owner
         }
         private IWebElement _menuNameField => driver.FindElement(By.Name("Menu Name"));
         private IWebElement _listMenuButton => driver.FindElement(By.XPath("//span[text() = 'List menu']"));
-        private IWebElement _imageMenuButton => driver.FindElement(By.XPath("//span[text() = 'List menu']"));
-        private IWebElement _nextButton => driver.FindElement(By.XPath(" //span[text() = 'Next']"));
+        private IWebElement _imageMenuButton => driver.FindElement(By.XPath("//span[text() = 'Image menu']"));
+        private IWebElement _nextButton => driver.FindElement(By.XPath("//legend[contains(text(),'Choose menu type:')]/parent::div/parent::div/following-sibling::div[2]"));
 
         public CreateMenuPage ClickMenuField()
         {
             _menuNameField.Click();
+            return this;
+        }
+        public CreateMenuPage SendKeysToMenuNameField(string name)
+        {
+            _menuNameField.SendKeys(name);
             return this;
         }
         public CreateMenuPage ClickListMenuButton()
@@ -33,10 +39,10 @@ namespace TestFramework.Pages.Owner
             _imageMenuButton.Click();
             return this;
         }
-        public CreateMenuPage ClickNextButton()
+        public CreateListMenuComponent ClickNextButton()
         {
             _nextButton.Click();
-            return this;
+            return new CreateListMenuComponent(driver);
         }
     }
 }
