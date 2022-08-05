cf9d84fc9b09b54acc910eafab6ff2800d2ffd83
LoveMyDaughter <tsav1986@gmail.com>
2022-08-05 22:48:32 +0300

    Remove excessive method

--- TAF_EasyRest/TestFramework/PageComponents/Moderator/UnapprovedRestaurantsPageComponent .cs
+++ TAF_EasyRest/TestFramework/PageComponents/Moderator/UnapprovedRestaurantsPageComponent .cs
@@ -2,7 +2,7 @@
 {
     public class UnapprovedRestaurantsPageComponent
     {
-        private IWebDriver driver;
+        private IWebDriver driver { get; }
 
         public UnapprovedRestaurantsPageComponent(IWebDriver driver)
         {
@@ -12,26 +12,14 @@
         private IReadOnlyCollection<IWebElement> _restaurantsGrid => driver.FindElements(By.XPath("//div[contains(@class, 'Grid-item')]"));
 
 
-
         private IWebElement _approveButton => driver.FindElement(By.XPath("//span[text() = 'Approve']/parent::button"));
 
         public UnapprovedRestaurantsPageComponent ClickApproveButton()
         {
             var firstRestaurantFromGrid = _restaurantsGrid.ElementAt(0);
             var approveButton = firstRestaurantFromGrid.FindElement(By.XPath(".//span[text() = 'Approve']/parent::button"));
-
             approveButton.Click();
-
             return new UnapprovedRestaurantsPageComponent(driver);
         }
-
-
-
-
-        public ArchivedRestaurantsPageComponent FindAndClickRestoreButton()
-        {
-
-        }
-
     }
 }
