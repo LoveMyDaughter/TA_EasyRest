9bb93fae66246b069526e7c6d4f45a7787660ee2
LoveMyDaughter <tsav1986@gmail.com>
2022-08-06 07:52:40 +0300

    Add class ModeratorPanelPageComponent. This is a LeftsideMenu for moderator

---
+++ TAF_EasyRest/TestFramework/PageComponents/Moderator/ModeratorPanelPageComponent.cs
@@ -0,0 +1,45 @@
+﻿namespace TestFramework.PageComponents.Moderator
+{
+    public class ModeratorPanelPageComponent
+    {
+        private IWebDriver driver { get; }
+
+        public ModeratorPanelPageComponent (IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+
+        #region Elements
+
+        private IWebElement _restaurantsButton => driver.FindElement(By.XPath("//span[text() = 'Restaurants']"));
+        private IWebElement _usersButton => driver.FindElement(By.XPath("//span[text() = 'Users']"));
+        private IWebElement _ownersButton => driver.FindElement(By.XPath("//span[text() = 'Owners']"));
+
+        #endregion
+
+
+        #region Methods
+
+        public ModeratorPanelRestaurantsPage ClickRestaurantsButton()
+        {
+            _restaurantsButton.Click();
+            return new ModeratorPanelRestaurantsPage(driver);
+        }
+
+        public ModeratorPanelUsersPage ClickUsersButton()
+        {
+            _usersButton.Click();
+            return new ModeratorPanelUsersPage(driver);
+        }
+
+        public ModeratorPanelOwnersPage ClickOwnersButton()
+        {
+            _ownersButton.Click();
+            return new ModeratorPanelOwnersPage(driver);
+        }
+
+        #endregion
+
+    }
+}
