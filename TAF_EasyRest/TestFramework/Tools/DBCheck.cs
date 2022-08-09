52a8dc6376471d6f50d9b6ca26f2f8aa2417500d
LoveMyDaughter <tsav1986@gmail.com>
2022-08-09 06:16:18 +0300

    Correct SignIn SignUp tests and TearDown

---
+++ TAF_EasyRest/TestFramework/Tools/DBCheck.cs
@@ -0,0 +1,8 @@
+﻿
+namespace TestFramework.Tools
+{
+    public class DBCheck
+    {
+        // Select * from table where ...
+    }
+}
--- TAF_EasyRest/TestFramework/Tools/DBCleanup.cs
+++ TAF_EasyRest/TestFramework/Tools/DBCleanup.cs
@@ -10,5 +10,15 @@
             DBConnectionWrapper.ExecuteQuery(deleteUser);
             // NoUser Exception...
         }
+
+        // If user logs out correctly - the token is removed automatically
+        public static void DeleteTokenAfterLogin(string userEmail)
+        {
+            string deleteUser = $"DELETE FROM users WHERE email = '{userEmail}'";
+            DBConnectionWrapper.ExecuteQuery(deleteUser);
+            // NoUser Exception...
+        }
+
+
     }
 }
