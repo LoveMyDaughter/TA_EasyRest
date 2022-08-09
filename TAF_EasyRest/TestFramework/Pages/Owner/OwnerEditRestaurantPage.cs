7bf6f1de2492281c85e896af050acd2971574b2d
Tetiana Rezhytska <tania.rezhytska@jmail.com>
2022-08-09 12:16:07 +0300

    Add _UserMenuHeaderButtonPageComponent

--- TAF_EasyRest/TestFramework/Pages/Owner/OwnerEditRestaurantPage.cs
+++ TAF_EasyRest/TestFramework/Pages/Owner/OwnerEditRestaurantPage.cs
@@ -9,12 +9,14 @@ public class OwnerEditRestaurantPage : BasePage
     public NavigationMenuPageComponent NavigationMenuPageComponent { get; }
     public ManageRestaurantPageComponent ManageRestaurantPageComponent { get; }
     public NewRestaurantPageComponet NewRestaurantPageComponet { get; }
+    public UserMenuHeaderButtonPageComponent UserMenuHeaderButtonPageComponent { get; }
 
     public OwnerEditRestaurantPage(IWebDriver driver) : base(driver)
     {
         NavigationMenuPageComponent = new NavigationMenuPageComponent(driver);
         ManageRestaurantPageComponent = new ManageRestaurantPageComponent(driver);
         NewRestaurantPageComponet = new NewRestaurantPageComponet(driver);
+        UserMenuHeaderButtonPageComponent = new UserMenuHeaderButtonPageComponent(driver);
     }
 
     private IWebElement _editInformationButton => driver.FindElement(By.XPath("//button[@title = 'Edit Information']"));
