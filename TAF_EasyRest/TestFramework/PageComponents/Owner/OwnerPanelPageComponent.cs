cf2bfb49170091342457ac54bc189a17327e5d80
Tetiana Rezhytska <tania.rezhytska@jmail.com>
2022-08-12 13:04:31 +0300

    Extracted WaiterItemPageComponent and \restaurantItemPageComponent for Owner page.

--- TAF_EasyRest/TestFramework/PageComponents/Owner/OwnerPanelPageComponent.cs
+++ TAF_EasyRest/TestFramework/PageComponents/Owner/OwnerPanelPageComponent.cs
@@ -13,16 +13,17 @@ namespace TestFramework.PageComponents
         private IWebElement _manageButton => driver.FindElement(By.XPath("//span[text() = 'Manage']/ancestor::a"));
         private IWebElement _archiveButton => driver.FindElement(By.XPath("//span[text() = 'Archive']/ancestor::li"));
 
-        public OwnerEditRestaurantPage ClickSignInButton()
+        public OwnerEditRestaurantPage ClickManageButton()
         {
             _manageButton.Click();
+            Thread.Sleep(1000);
             return new OwnerEditRestaurantPage(driver);
         }
 
-        public OwnerPanelRestaurantsPage ClickSignUpButton()
+        public OwnerPanelPageComponent ClickArchiveButton()
         {
             _archiveButton.Click();
-            return new OwnerPanelRestaurantsPage(driver);
+            return new OwnerPanelPageComponent(driver);
         }
     }
 }
