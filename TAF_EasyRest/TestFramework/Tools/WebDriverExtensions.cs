8bdd5236969e27f64851e16323036fb1fc302e2e
Tetiana Rezhytska <tania.rezhytska@jmail.com>
2022-08-13 21:21:11 +0300

    RemoveWaiter test adjusted to working correctly when there is one waiter.

---
+++ TAF_EasyRest/TestFramework/Tools/WebDriverExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace TestFramework.Tools
+{
+    public static class WebDriverExtensions
+    {
+        public static IWebElement FindElementSafe(this IWebDriver driver, By by)
+        {
+            try
+            {
+                return driver.FindElement(by);
+            }
+            catch (NoSuchElementException)
+            {
+                return null;
+            }
+        }
+    }
+}
