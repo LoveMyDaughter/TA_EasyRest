9515b8fcaaf07dff4a09c285aa0eb7c4aec85010
andriifromlviv <a.shalavylo@gmail.com>
2022-08-15 13:11:29 +0300

    shorten overloaded method WaitUntilElementIsVisible

--- TAF_EasyRest/TestFramework/PageComponents/BasePageComponent.cs
+++ TAF_EasyRest/TestFramework/PageComponents/BasePageComponent.cs
@@ -4,21 +4,19 @@ namespace TestFramework.PageComponents
 {
     public class BasePageComponent
     {
-        protected IWebElement WaitUntilElementIsVisible(By locator, IWebElement parent, int timeToWait)
-        {
-            var driver = parent.GetWebDriverFromWebElement();
-
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeToWait));
-            var el = wait.Until(ExpectedConditions.ElementIsVisible(locator));
-            return el;
-        }
-
         protected IWebElement WaitUntilElementIsVisible(By locator, IWebDriver driver, int timeToWait)
         {
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeToWait));
             var el = wait.Until(ExpectedConditions.ElementIsVisible(locator));
             return el;
         }
+        
+        protected IWebElement WaitUntilElementIsVisible(By locator, IWebElement parent, int timeToWait)
+        {
+            var driver = parent.GetWebDriverFromWebElement();
+            return WaitUntilElementIsVisible(locator, driver, timeToWait);
+        }
+
 
         protected void WaitUntilCollectionIsFilled(IReadOnlyCollection<IWebElement> elements, IWebDriver driver, int timeToWait)
         {
