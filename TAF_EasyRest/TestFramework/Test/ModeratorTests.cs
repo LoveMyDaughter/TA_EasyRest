4074893a27c7a011f4f59f2dc59e2439e1c4f342
LoveMyDaughter <tsav1986@gmail.com>
2022-08-18 09:17:39 +0300

    Create class ModeratorTests. Create test ChangeUserStatusTest

---
+++ TAF_EasyRest/TestFramework/Test/ModeratorTests.cs
@@ -0,0 +1,45 @@
+﻿using TestFramework.Pages.Moderator;
+
+namespace TestFramework.Test
+{
+    public class ModeratorTests : BaseTest
+    {
+        private static string userEmail = "petermoderator@test.com";
+        private static string userPassword = "1";
+        IWebDriver driver = new ChromeDriver();
+
+
+        [OneTimeSetUp]
+        public void BeforeModeratorsTests()
+        {
+            UserLogin(driver, userEmail, userPassword);
+        }
+
+        [Test]
+        [Category("Smoke")]
+        [Category("Positive")]
+        //[Repeat(2)]  // This restores the previous user's status (postcondition of test case)
+        public void ChangeUserStatusTest()
+        {
+            // Arrange
+            ModeratorPanelUsersPage moderatorPanelUsersPage = new ModeratorPanelUsersPage(driver);
+            moderatorPanelUsersPage.GoToUrl(); Thread.Sleep(1000);
+            string initialStatus = moderatorPanelUsersPage.ShowUserStatus();
+
+            // Act
+            moderatorPanelUsersPage.FindAndClickPadlockButton(); Thread.Sleep(1000);
+            string finalStatus = moderatorPanelUsersPage.ShowUserStatus();
+
+            // Assert
+            Assert.That(initialStatus != finalStatus);
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllTests()
+        {
+            UserLogout(userEmail);
+            driver.Quit();
+        }
+
+    }
+}
