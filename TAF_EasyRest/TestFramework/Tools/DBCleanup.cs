602eafa41ef119cc39c40406c1d3e84827ab4676
LoveMyDaughter <tsav1986@gmail.com>
2022-08-23 23:07:31 +0300

    Add method for manual deleting restaurant via DB

--- TAF_EasyRest/TestFramework/Tools/DBCleanup.cs
+++ TAF_EasyRest/TestFramework/Tools/DBCleanup.cs
@@ -14,5 +14,11 @@
             string deleteToken = $"delete from tokens where id = (SELECT tokens.id FROM tokens JOIN users ON tokens.user_id = users.id WHERE users.email = '{userEmail}' order by id desc limit 1)";
             DBConnectionWrapper.ExecuteQuery(deleteToken);
         }
+
+        public static void DeleteRestaurantByName(string restaurantName)
+        {
+            string deleteRest = $"DELETE FROM restaurants WHERE name = '{restaurantName}'";
+            DBConnectionWrapper.ExecuteQuery(deleteRest);
+        }
     }
 }
