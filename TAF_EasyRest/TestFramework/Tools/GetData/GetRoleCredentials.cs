17d548242c405c3a19e67e8c6ad5c86b21d41b23
sofiaso <sofiaso.sofiasoo@gmail.com>
2022-08-23 13:57:46 +0300

    add serialization & deserialization

---
+++ TAF_EasyRest/TestFramework/Tools/GetData/GetRoleCredentials.cs
@@ -0,0 +1,34 @@
+﻿//using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace TestFramework.Tools.deserialization
+{
+    public class GetRoleCredentials
+    {
+        private static string _roleCredentials = Directory.GetParent(path:@"../../") + "RolesCredentials.json";
+
+        public static void Serialize(List<RoleCredentials> data)
+        {
+            using (FileStream fs = new FileStream(_roleCredentials, FileMode.OpenOrCreate))
+            {
+                JsonSerializer.Serialize(fs, data);
+            }
+        }
+
+        private static List<RoleCredentials> getListRoleCredentials()
+        {
+            List<RoleCredentials> roles;
+            using (FileStream fs = new FileStream(_roleCredentials, FileMode.OpenOrCreate))
+            {
+                roles = JsonSerializer.Deserialize<List<RoleCredentials>>(fs);
+            }
+
+            return roles;
+        }
+
+        public static RoleCredentials GetCredentials(string roleName)
+        {
+            return getListRoleCredentials().Find(role => role.RoleName == roleName);
+        }
+    }
+}
---
+++ TAF_EasyRest/TestFramework/Tools/GetData/RoleCredentials.cs
@@ -0,0 +1,16 @@
+﻿namespace TestFramework.Tools.deserialization
+{
+    public class RoleCredentials
+    {
+        public RoleCredentials(string RoleName, string Email, string Password)
+        {
+            this.RoleName = RoleName;
+            this.Email = Email;
+            this.Password = Password;
+        }
+
+        public string RoleName { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+    }
+}
