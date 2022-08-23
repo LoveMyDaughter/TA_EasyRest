ebe546ae552affd3ce97540a4ecb84b6c6c89966
LoveMyDaughter <tsav1986@gmail.com>
2022-08-23 23:06:09 +0300

    Add method for manual creating restaurant via DB

---
+++ TAF_EasyRest/TestFramework/Tools/DBAddition.cs
@@ -0,0 +1,14 @@
+﻿
+namespace TestFramework.Tools
+{
+    public class DBAddition
+    {
+
+        public static void AddRestaurantViaDB(string name, string address = "3807 Brook Street, Huston, TX 77030, USA", int ownerId = 1, int status = 0)
+        {
+            string addRestaurant = $"INSERT INTO restaurants (name, address_id, owner_id, status) VALUES ('{name}', '{address}', {ownerId}, {status})";
+            DBConnectionWrapper.ExecuteQuery(addRestaurant);
+        }
+
+    }
+}
