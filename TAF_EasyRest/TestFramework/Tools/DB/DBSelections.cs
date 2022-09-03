cebef66567eadac5da5f70e69e8c84a8e939d6f1
LoveMyDaughter <tsav1986@gmail.com>
2022-09-03 22:16:27 +0300

    Correct possibly-nullable values in DBSelections

--- TAF_EasyRest/TestFramework/Tools/DB/DBSelections.cs
+++ TAF_EasyRest/TestFramework/Tools/DB/DBSelections.cs
@@ -8,14 +8,14 @@
         public static int GetOrdersCountByStatus(string userEmail, string orderStatus = "Waiting for confirm")
         {
             string queryString = $"SELECT count(*) FROM orders WHERE status = '{orderStatus}' AND user_id = (SELECT id FROM users WHERE email = '{userEmail}') ";
-            object ordersCount = DBConnectionWrapper.GetCellFromDB(queryString);
+            object? ordersCount = DBConnectionWrapper.GetCellFromDB(queryString);
             return Convert.ToInt32(ordersCount);
         }
 
         public static int GetLastOrderIdByStatus(string orderStatus = "Waiting for confirm")
         {
             string queryString = $"SELECT id FROM orders WHERE status = '{orderStatus}' ORDER BY id DESC LIMIT 1";
-            object orderId = DBConnectionWrapper.GetCellFromDB(queryString);
+            object? orderId = DBConnectionWrapper.GetCellFromDB(queryString);
             return Convert.ToInt32(orderId);
         }
         #endregion
