e1c6ac6f816d5d4ca8b4d008fff61cc545567b0b
LoveMyDaughter <tsav1986@gmail.com>
2022-09-05 21:42:04 +0300

    Create class AdminTests

---
+++ TAF_EasyRest/TestFramework/Test/AdminTests.cs
@@ -0,0 +1,34 @@
+﻿namespace TestFramework.Test
+{
+    public class AdminTests : BaseTest
+    {
+        public IWebDriver driver { get; private set; }
+
+        [OneTimeSetUp]
+        public void BeforeAdminsTests()
+        {
+            driver = new ChromeDriver();
+            userEmail = GetRoleCredentials.GetCredentials("Admin").Email;
+            userPassword = GetRoleCredentials.GetCredentials("Admin").Password;
+            UserLogin(driver, userEmail, userPassword);
+        }
+
+        [Test]
+        [Category("Smoke")]
+        [Category("Positive")]
+        public void ChangeModeratorStatus()
+        {
+            Assert.Fail();
+        }
+
+
+        [OneTimeTearDown]
+        public void AfterAdminsTests()
+        {
+            driver.Quit();
+            UserLogout(userEmail);
+        }
+
+
+    }
+}
