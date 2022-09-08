38c3c0f59ff95a07d969463135cec42d9a94b27d
LoveMyDaughter <tsav1986@gmail.com>
2022-09-08 17:13:09 +0300

    Add method CreateUser in BaseTest class

--- TAF_EasyRest/TestFramework/Test/BaseTest.cs
+++ TAF_EasyRest/TestFramework/Test/BaseTest.cs
@@ -43,6 +43,11 @@ namespace TestFramework.Test
         {
             DBAddition.AddUserViaDB("0 Fake Moderator", email, 3, true);
         }
-         
+
+        public void CreateUser(string email)
+        {
+            DBAddition.AddUserViaDB("0 Fake User", email, 1, true);
+        }
+
     }
 }
